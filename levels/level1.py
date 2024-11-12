4ed579d4f433c97ea7e9bb536c26ff68db05278a
mrgw21 <mrgw21@bath.ac.uk>
2024-11-12 17:02:57 +0000

    feat: initialize spawn and collision functions on level1

--- levels/level1.py
+++ levels/level1.py
@@ -1,5 +1,6 @@
 import pygame
 from objects.cell import Cell
+from objects.macrophage import Macrophage
 
 class Level1:
     def __init__(self, screen):
@@ -7,7 +8,14 @@ class Level1:
         self.clock = pygame.time.Clock()
         self.running = True
         self.body_image = pygame.image.load('assets/images/body_placeholder.png')
+
+        self.macrophage = Macrophage()
         self.cells = [Cell(i) for i in range(37)] 
+
+        self.enemies = []
+
+        self.spawn_timer = 0
+        self.spawn_interval = 2000
 
     def run(self):
         while self.running:
@@ -18,19 +26,42 @@ class Level1:
                 if event.type == pygame.FULLSCREEN:
                     pygame.display.flip()
                 """
-            self.screen.fill((0, 0, 0))
+            self.screen.fill((255, 255, 255))
             self.clock.tick(60)
             self.draw()
+
+    def spawn_enemy(self):
+        if pygame.time.get_ticks() - self.spawn_timer > self.spawn_interval:
+            if pygame.time.get_ticks() % 2 == 0:
+                # Bacteria
+                self.enemies.append(None)
+            else:
+                # Virus
+                self.enemies.append(None)
+            self.spawn_timer = pygame.time.get_ticks()
     
+    def check_collisions(self):
+        for enemy in self.enemies[:]:
+            if self.macrophage.rect.colliderect(enemy.rect):
+                self.macrophage.eat(enemy)
+                self.enemies.remove(enemy)
+
+            # Check if enemy reached the cells
+            for cell in self.cells:
+                if enemy.rect.colliderect(cell.rect):
+                    cell.die()
+                    self.enemies.remove(enemy)
+                    break
+
     def draw(self):
-        # Get the rect for positioning the image (you can adjust position and size here)
-        # Center the image at (400, 300)
-        
-        # Blit the image to the screen at the given position
+
+        self.macrophage.update()
+
         img = self.body_image
         img = pygame.transform.scale(img, (img.get_width() * 0.32, img.get_height() * 0.32))
         body_rect = img.get_rect(center=(400, 300)) 
         self.screen.blit(img, body_rect)
         for cell in self.cells:
             cell.draw(self.screen)
+        self.macrophage.draw(self.screen)
         pygame.display.flip()  # Update the screen with the new drawing
---
+++ objects/macrophage.py
@@ -0,0 +1,32 @@
+import pygame
+
+class Macrophage:
+    
+    def __init__(self):
+        self.image = pygame.image.load("assets/images/macrophage_placehoder.png")
+        self.rect = self.image.get_rect(center=(300, 350))
+        img = self.image
+        self.image = pygame.transform.scale(img, (img.get_width() * 0.3, img.get_height() * 0.3))
+        self.speed = 5
+        # self.strength = 0
+    
+    def handle_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s]:
+            self.rect.y += self.speed
+        if keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_d]:
+            self.rect.x += self.speed
+    
+    def eat(self, enemy):
+        # Logic to "eat" an enemy, making the macrophage stronger
+        pass
+    
+    def update(self):
+        self.handle_input()
+    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
